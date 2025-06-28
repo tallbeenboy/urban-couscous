df9f6a4224e1100cea34f8c72ed61926e261b0e9
tallbeenboy <120860036+tallbeenboy@users.noreply.github.com>
2025-06-28 16:32:45 -0700

    Update app.py

--- app.py
+++ app.py
@@ -197,3 +197,9 @@ def buy():
         return jsonify("failed: firestore error")
 
     return jsonify(f"success: bought {shares} {symbol} at {price}")
+
+
+if __name__ == '__main__':
+    port = int(os.environ.get('PORT', 10000))
+    app.run(host='0.0.0.0', port=port, debug=True)
+
