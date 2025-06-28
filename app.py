d39cfbad06f5df335516e5f4cef3beeb260f92c6
tallbeenboy <120860036+tallbeenboy@users.noreply.github.com>
2025-06-28 16:16:17 -0700

    Update app.py

--- app.py
+++ app.py
@@ -332,5 +332,5 @@ def test_firestore():
 
 
 if __name__ == '__main__':
-    port = int(os.environ.get('PORT', 5000))  # default to 5000 locally
+    port = int(os.environ.get('PORT', 10000))  # default to 5000 locally
     app.run(host='0.0.0.0', port=port, debug=True)
